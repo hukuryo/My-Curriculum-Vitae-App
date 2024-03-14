f82a50a66247608250573594d3b3bd86a954744f
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-14 22:36:00 +0900

    リストのデザインを改良

--- components/ZennArticleList.tsx
+++ components/ZennArticleList.tsx
@@ -5,23 +5,24 @@ import { ArticleProps } from "@/app/types/articleType";
 
 export const ZennArticleList = ({ articles }: { articles: ArticleProps[] }) => {
   return (
-    <div>
+    <ul className="grid place-items-center gap-7 items-stretch grid-cols-[repeat(auto-fit,minmax(240px,1fr))]">
       {articles.map((article) => (
-        <Link
-          href={`https://zenn.dev/${article.path}`}
-          className="text-blue-500 hover:text-blue-700"
-        >
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <div className="px-6 py-4">
-              <div className="text-gray-700 text-base">{article.emoji}</div>
-              <div className="font-bold text-xl mb-2">{article.title}</div>
-              <p className="text-gray-700 text-base">
-                {dayjs(article.published_at).format("YYYY/MM/DD")}
-              </p>
-            </div>
-          </div>
-        </Link>
+        <li className="w-full">
+          <Link
+            target="blank"
+            href={`https://zenn.dev/${article.path}`}
+            className="flex aspect-[4/3] w-full h-full flex-col items-center justify-center gap-4 bg-background p-6 border rounded-3xl hover:outline-primary hover:outline-2 hover:outline-double hover:bg-muted/60"
+          >
+            <span className="text-6xl">{article.emoji}</span>
+            <p className="line-clamp-2 overflow-hidden break-all text-left font-medium">
+              {article.title}
+            </p>
+            <p className="text-muted-foreground text-xs tracking-widest">
+              {dayjs(article.published_at).format("YYYY/MM/DD")}
+            </p>
+          </Link>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
