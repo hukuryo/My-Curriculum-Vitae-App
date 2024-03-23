a425290b912db439c852a1221c7944bbad7b0127
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-23 21:42:27 +0900

    ヘッダーのデザインを調整

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 import { ModeToggle } from "./ui/toggle";
 import Link from "next/link";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/app/components/ui/button";
 
 export const Header = () => {
   return (
@@ -13,15 +16,24 @@ export const Header = () => {
             CURRICULUM VITAE
           </h1>
         </Link>
+        <div className="flex-grow flex justify-center items-center gap-5">
+          <div className="flex gap-8">
+            <Link className="hover:opacity-75" href="/qiita">
+              Qiita
+            </Link>
+            <Link className="hover:opacity-75" href="/zenn">
+              Zenn
+            </Link>
+            <Link className="hover:opacity-75" href="/career">
+              CAREER
+            </Link>
+          </div>
+        </div>
         <div className="flex items-center gap-5">
-          <Link className="hover:opacity-75" href="/qiita">
-            Qiita
-          </Link>
-          <Link className="hover:opacity-75" href="/zenn">
-            Zenn
-          </Link>
-          <Link className="hover:opacity-75" href="/career">
-            CAREER
+          <Link className="hover:opacity-75" href="https://github.com/hukuryo">
+            <Button variant="outline" size="icon">
+              <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
           </Link>
           <ModeToggle />
         </div>
