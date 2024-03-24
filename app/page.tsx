637e71d9f95ba94d2f0a366817dc0c0dd68aaec8
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-24 19:57:44 +0900

    トップページのレイアウトを修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-6">
-      <header className="mb-12">
+    <>
+      <div className="mb-12">
         <h1 className="text-4xl font-bold text-center">
           Welcome to My PortFolio Site
         </h1>
-      </header>
+      </div>
       <section className="mb-12">
         <p className="text-lg text-center">
           自分が勉強したことを発信しているサイトをまとめました。
         </p>
       </section>
-      <footer className="mt-12">
+      <div className="mt-12">
         <p className="text-sm text-center">© 2024 All rights reserved.</p>
-      </footer>
-    </main>
+      </div>
+    </>
   );
 }
