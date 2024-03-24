d033c301139c4bf5b66937cc53c4e746dd50981d
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-24 19:47:59 +0900

    トップページを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-center min-h-screen p-6">
+      <header className="mb-12">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to My PortFolio Site
+        </h1>
+      </header>
+      <section className="mb-12">
+        <p className="text-lg text-center">
+          自分が勉強したことを発信しているサイトをまとめました。
+        </p>
+      </section>
+      <footer className="mt-12">
+        <p className="text-sm text-center">© 2024 All rights reserved.</p>
+      </footer>
+    </main>
   );
 }
