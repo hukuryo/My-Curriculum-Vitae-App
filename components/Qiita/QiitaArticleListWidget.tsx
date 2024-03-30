4c58e291726dcaab024cd6e55ec9e21c5c00a511
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-30 20:15:33 +0900

    Qiitaの記事の一覧を表示する実装途中

--- components/Qiita/QiitaArticleListWidget.tsx
+++ components/Qiita/QiitaArticleListWidget.tsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { QiitaArticleList } from "./QiitaArticleList";
-import { ArticleProps } from "@/app/types/articleType";
 
-export const QiitaArticleListWidget: React.FC = () => {
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const userName = "hukuryo";
-      const url = `https://qiita.com/api/v2/items?page=1&per_page=100&query=user:${userName}`;
-      const response = await fetch(url);
-      const data = await response.json();
-    };
+export const QiitaArticleListWidget = async () => {
+  const userName = "hukuryo";
+  const url = `https://qiita.com/api/v2/items?page=1&per_page=100&query=user:${userName}`;
 
-    fetchArticles();
-  }, []);
+  const response = await fetch(url, { cache: "force-cache" });
+  console.log(response);
+  const articles = await response.json();
 
   return (
     <>
