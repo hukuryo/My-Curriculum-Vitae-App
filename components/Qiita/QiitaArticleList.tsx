659c905baa764a34a9d7f42c5d73d6baab2f3ab2
hukuryo <ryohei.1998.05.09@gmail.com>
2024-04-03 23:02:06 +0900

    Qiitaの記事の一覧のデザインをやり直し

--- components/Qiita/QiitaArticleList.tsx
+++ components/Qiita/QiitaArticleList.tsx
@@ -1,63 +1,11 @@
 import React from "react";
 import { ArticleProps } from "@/app/types/articleType";
+import Link from "next/link";
 
 export const QiitaArticleList = ({
   articles,
 }: {
   articles: ArticleProps[];
 }) => {
-  return (
-    <div className="bg-gray-100 min-h-screen py-10">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-3xl font-bold mb-6">Qiita Articles</h1>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {articles.map((article: ArticleProps) => (
-            <div
-              key={article.id}
-              className="bg-white rounded-lg shadow-md overflow-hidden"
-            >
-              <div className="p-6">
-                <h2 className="text-xl font-semibold mb-2">{article.title}</h2>
-
-                <div className="flex items-center">
-                  <span className="text-gray-700">
-                    {article.id} -{" "}
-                    {new Date(article.published_at).toLocaleDateString()}
-                  </span>
-                </div>
-              </div>
-              <div className="px-6 py-4 bg-gray-100">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <svg
-                      className="w-5 h-5 text-gray-500 mr-2"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                  </div>
-                  <a
-                    href={article.path}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:text-blue-700"
-                  >
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="bg-gray-100 min-h-screen py-10"></div>;
 };
